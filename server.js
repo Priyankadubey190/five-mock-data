462a134ca532750f8462fd1a3d5758bb352251ab
Priyankadubey190 <priyankadubey0811@gmail.com>
2023-01-05 14:40:40 +0530

    first second

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
+server.use((req, res,next) => {
+res.header("Access-Control-Allow-Origin", "*");
+next();
+})
+
 server.use(middlewares);
 server.use(router);
 
