4df6fdb6c2645d6bdda51da1173982d3dde67c1e
Priyankadubey190 <priyankadubey0811@gmail.com>
2023-01-06 10:13:06 +0530

    final

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+require('dotenv').config()
+const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 3001
 
-server.use((req, res,next) => {
-res.header("Access-Control-Allow-Origin", "*");
-next();
-})
+// server.use((req, res,next) => {
+// res.header("Access-Control-Allow-Origin", "*");
+// next();
+// })
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port,() => {
+    console.log(`Server is running at ${port}`)
+  });
